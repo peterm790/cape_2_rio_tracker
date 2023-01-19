fa76f71a52a391c8e8357d71aa54d7d6055e27ed
peterm790 <petermarsh790@gmail.com>
2023-01-19 23:14:09 +0200

    latest

--- Home.py
+++ Home.py
@@ -6,13 +6,6 @@ import streamlit.components.v1 as components
 import datetime
 
 
-st.set_page_config(layout="wide",
-                page_title="Pete's Rio Tracker",
-                page_icon = ":wave:",
-)
-
-st.title(' Cape To Rio Tracker')
-
 name_dic = {'Alexforbes ArchAngel': 'Alexforbes_ArchAngel',
             'Argonaut': 'Argonaut',
             'Atalanta': 'Atalanta',
@@ -25,49 +18,97 @@ name_dic = {'Alexforbes ArchAngel': 'Alexforbes_ArchAngel',
             'Sterna': 'Sterna',
             'Translated 9': 'Translated_9'}
 
-
-option = st.selectbox(
-    'View',
-    ('Leaderboard', 'Routing Maps'),
-    help = '')
-    
-
-if option == 'Leaderboard':
-    fs = fsspec.filesystem('')
-    files = fs.glob('/home/peter/Documents/rio_tracker/results/*.json')
-    names = [' '.join(file.split('/')[-1].split('.')[0].split('_')) for file in files]
+@st.cache()
+def get_leaderboard():
     data = []
-    for file, name in zip(files,names):
+    for name in names:
+        file = f"s3://riotrackerlambdastack-cape2riotrackingbucket493cd-ax0w0veyvbks/results/{name_dic[name]}.json"
         with fs.open(file, 'r') as f:
             data.append(pd.DataFrame(json.load(f), index = [name]))
     df = pd.concat(data).iloc[:,1:]
-    df.columns = ['ETA', 'ORC handicap', 'Estimated Time Elapsed', 'Estimated Corrected Time']
-    df.sort_values('Estimated Corrected Time')
+    df.columns = ['ETA', 'IRC handicap', 'Estimated Time Elapsed', 'Estimated Corrected Time']
+    return df.sort_values('Estimated Corrected Time')
+
+@st.cache()
+def get_html(name):
+    file = f"s3://riotrackerlambdastack-cape2riotrackingbucket493cd-ax0w0veyvbks/results/{name_dic[name]}.html"
+    with fs.open(file, 'rb', encoding = 'utf-8') as f:
+        HtmlFile = f.read()
+    return HtmlFile
+
+@st.cache()
+def get_roadbook(name):
+    file = f"s3://riotrackerlambdastack-cape2riotrackingbucket493cd-ax0w0veyvbks/results/{name_dic[name]}.csv"
+    with fs.open(file, 'r') as f:
+        road_book = pd.read_csv(f)[['time', 'lat', 'lon', 'twd', 'tws', 'heading', 'twa','boat_speed', 'days_elapsed']]
+    start = datetime.datetime(2023,1,2,12)
+    current = datetime.datetime.today().replace(hour=7, minute=0, second = 0, microsecond=0)
+    elapsed = current - start
+    road_book.days_elapsed = road_book.days_elapsed + elapsed.days-0.20 #start was at 1400
+    return road_book
+
+
+st.set_page_config(layout="wide",
+                page_title="Pete's Rio Tracker",
+                page_icon = ":sailboat:",
+)
+
+st.title(":blue[Pete's Cape To Rio Tracker] :flag-za: -> :flag-br:")
+
+st.write(':green[The Tracker will update at 9:30 am SAST daily.]')
+
+option = st.selectbox(
+    'View:',
+    ('Leaderboard', 'Routing Maps'),
+    help = '')
+
+fs = fsspec.filesystem('s3')
+names = list(name_dic)
+
+if option == 'Leaderboard':
+    df = get_leaderboard()
     st.dataframe(df, use_container_width=True)
+    st.write(':blue[This forecasted leaderboard has been determined by weather routing each yacht individually to the finish using a polar file derived from the ORC handicap. It represents the expected finishing order if each team were to sail at 100% of their handicap from now until the finish. I have used IRC to determine the final rankings as this is what will be used to determine the overall winner.]')
+    st.write(':blue[The rankings here may differ significantly from the official tracker as that determines the final rankings by extrapolating each boats VMG, usually favoring those taking the mose direct course.]')
+    st.write(':blue[All times are UTC]')
 elif option == 'Routing Maps':
-    fs = fsspec.filesystem('')
-    files = fs.glob('/home/peter/Documents/rio_tracker/results/*.html')
-    names = [' '.join(file.split('/')[-1].split('.')[0].split('_')) for file in files]
     team = st.selectbox(
-                        'Team',
+                        'Team:',
                         names,
                         help = '')
     map = st.selectbox(
-                    'View',
+                    'Style:',
                     ('Routing Chart', 'Road Book'),
                     help = '')
-    dic = {names[i]: files[i] for i in range(len(files))}
     if map == 'Routing Chart':
-        with fs.open(dic[team], 'rb', encoding = 'utf-8') as f:
-            HtmlFile = f.read()
+        st.write('Please note these maps do not render nicely on mobile devices')
+        HtmlFile = get_html(team)
         components.html(HtmlFile, height=900, scrolling = False)
+        st.write(':blue[The weather displayed here is the latest 00z GFS forecast]')
     elif map == 'Road Book':
-        csv_url = f"{dic[team].split('.')[0]}.csv"
-        with fs.open(csv_url, 'r') as f:
-            road_book = pd.read_csv(f)[['time', 'lat', 'lon', 'twd', 'tws', 'heading', 'twa','boat_speed', 'days_elapsed']]
-        start = datetime.datetime(2023,1,2,14)
-        current = datetime.datetime.today().replace(hour=9, minute=0, second = 0, microsecond=0)
-        elapsed = current - start
-        road_book.days_elapsed = road_book.days_elapsed + elapsed.days-0.20 #start was at 1400
+        road_book = get_roadbook(team)
         st.dataframe(road_book, use_container_width=True)
+        st.write(":blue[The polar filed used to calculate the expected route has been derived from the yacht's ORC handicap]")
 
+
+st.write(':red[This tracker has no affiliation with Cape to Rio Race, or the official YellowBrick tracker.]')
+
+
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+
+st.write('For any enquires contact me at petermarsh790@gmail.com')
